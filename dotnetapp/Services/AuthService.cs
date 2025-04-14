15b814b26c1bd6e7fd34742257fe018cda99854e
Gagan-Yadav <gagan.yadav-ic-2k19-27@iips.edu.in>
2025-04-14 15:56:11 +0000

    new

--- dotnetapp/Services/AuthService.cs
+++ dotnetapp/Services/AuthService.cs
@@ -98,7 +98,7 @@ namespace dotnetapp.Services
  
         private string GenerateToken(IEnumerable<Claim> claims)
         {
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:SecretKey"]));
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
             var token = new JwtSecurityToken(
                 issuer: _configuration["JWT:Issuer"],
