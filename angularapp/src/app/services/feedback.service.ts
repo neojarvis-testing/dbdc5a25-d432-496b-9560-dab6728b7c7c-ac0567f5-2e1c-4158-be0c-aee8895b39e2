827e6d8dc8dd8561941cd832f3498f3164417f3c
mayankmg18 <mgofficial18@gmail.com>
2025-04-16 07:14:07 +0000

    do commit n merge to main

--- angularapp/src/app/services/feedback.service.ts
+++ angularapp/src/app/services/feedback.service.ts
@@ -8,12 +8,12 @@ import { Feedback } from '../models/feedback.model';
 })
 export class FeedbackService {
 
-  private baseUrl = 'https://8080-dedadddddbafecbafcedadafebfecdebbceacfecbecaeebe.premiumproject.examly.io';
+  private baseUrl = 'https://ide-ceaeccbebfffaedadafebfecdebbceacfecbecaeebe.premiumproject.examly.io/proxy/8080';
 
   constructor(private http: HttpClient) { }
 
   private getAuthHeaders(): HttpHeaders {
-    const token = localStorage.getItem('authToken');
+    const token = localStorage.getItem('token');
     return new HttpHeaders({
       'Authorization': `Bearer ${token}`
     });
