6ca676c74d4a4fb76556d5c165ec09e1d688dfc8
AJAY372037 <ajaymannam123@gmail.com>
2025-04-17 13:26:02 +0000

    New

--- angularapp/src/environments/environment.ts
+++ angularapp/src/environments/environment.ts
@@ -4,7 +4,7 @@
  
 export const environment = {
     production: false,
-    apiUrl: 'https://ide-ceaeccbebfffaedadafebfecdebbceacfecbecaeebe.premiumproject.examly.io/proxy/8080'
+    apiUrl: 'https://ide-cdebaaabaaceadafebfecdebbceacfecbecaeebe.premiumproject.examly.io/proxy/8080'
   };
    
   /*
