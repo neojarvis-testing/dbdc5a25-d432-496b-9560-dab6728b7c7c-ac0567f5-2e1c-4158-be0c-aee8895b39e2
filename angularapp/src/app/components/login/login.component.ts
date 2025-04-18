1d705f98c409a9d0ef7181f23646a10dd25df4d9
Gagan-Yadav <gagan.yadav-ic-2k19-27@iips.edu.in>
2025-04-18 06:49:31 +0000

    new login

--- angularapp/src/app/components/login/login.component.ts
+++ angularapp/src/app/components/login/login.component.ts
@@ -87,4 +87,9 @@ export class LoginComponent implements OnInit {
   register(): void {
     this.router.navigate(['/register']);
   }
+  validateEmail(email: string): boolean {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailRegex.test(email);
+  }
+  
 }
