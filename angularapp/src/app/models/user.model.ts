2499b78f2e77f221a1ffa01dad29a76eec916216
mayankmg18 <mgofficial18@gmail.com>
2025-04-20 11:55:39 +0000

    j

--- angularapp/src/app/models/user.model.ts
+++ angularapp/src/app/models/user.model.ts
@@ -1,12 +1,8 @@
-public class User
-{
-    [Key]
-    public int UserId { get; set; }
-
-    public string Email { get; set; }
-    public string Password { get; set; }
-    public string Username { get; set; }
-    public string MobileNumber { get; set; }
-    public string UserRole { get; set; }
-    public string ProfileImageUrl { get; set; } // New
+export class User {
+    UserId?: number;
+    Email: string;
+    Password: string;
+    Username: string;
+    MobileNumber: string;
+    UserRole: string;
 }
